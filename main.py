4bacd9079d71e31771aaed23bf0996a88ac0c420
IvanowAndrew22md17 <125370213+IvanowAndrew22md17@users.noreply.github.com>
2023-05-30 07:09:22 +0300

    add

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ x = int(input("введите номер задания:"))
 #11.1
 def f1():
     class Restaurant:
-        def __init__(self, restaurant_name, cuisine_type):
-            self.restaurant_name = restaurant_name
-            self.cuisine_type = cuisine_type
-        def describe_restaurant(self):
-            print(f'Название ресторана: {self.restaurant_name} Тип кухни: {self.cuisine_type}')
+        def __init__(self, name, k_type):
+            self.name = name
+            self.k_type = k_type
+        def d_restaurant(self):
+            print(f'Название ресторана: {self.name} Тип кухни: {self.k_type}')
         def open_restaurant(self):
             print('Открыто с 12:00')
     newRestaurant = Restaurant(input(), input())
-    print(newRestaurant.restaurant_name)
-    print(newRestaurant.cuisine_type)
-    newRestaurant.describe_restaurant()
+    print(newRestaurant.name)
+    print(newRestaurant.k_type)
+    newRestaurant.d_restaurant()
     newRestaurant.open_restaurant()
     print('\n')
 if x == 1:
@@ -22,25 +22,25 @@ if x == 1:
 # 11.2
 def f2():
     class Restaurant:
-        def __init__(self, restaurant_name, cuisine_type):
-            self.restaurant_name = restaurant_name
-            self.cuisine_type = cuisine_type
-        def describe_restaurant(self):
-            print(f'Название ресторана: {self.restaurant_name} Тип кухни: {self.cuisine_type}')
+        def __init__(self, name, k_type):
+            self.name = name
+            self.k_type = k_type
+        def d_restaurant(self):
+            print(f'Название ресторана: {self.name} Тип кухни: {self.k_type}')
         def open_restaurant(self):
             print('Открыто с 12:00')
     newRestaurant = Restaurant(input(), input())
-    print(newRestaurant.restaurant_name)
-    print(newRestaurant.cuisine_type)
-    newRestaurant.describe_restaurant()
+    print(newRestaurant.name)
+    print(newRestaurant.k_type)
+    newRestaurant.d_restaurant()
     newRestaurant.open_restaurant()
     print('\n')
     Restaurant1 = Restaurant('Kuznyahouse', 'европейская, авторская')
     Restaurant2 = Restaurant('Co-Op Garage', 'европейская, американская, итальянская')
     Restaurant3 = Restaurant('Kiki', 'турецкая, авторская')
-    Restaurant1.describe_restaurant()
-    Restaurant2.describe_restaurant()
-    Restaurant3.describe_restaurant()
+    Restaurant1.d_restaurant()
+    Restaurant2.d_restaurant()
+    Restaurant3.d_restaurant()
     print('\n')
 if x==2:
     f2()
@@ -48,20 +48,20 @@ if x==2:
 #11.3
 def f3():
     class Restaurant:
-        def __init__(self, restaurant_name, cuisine_type, reiting):
-            self.restaurant_name = restaurant_name
-            self.cuisine_type = cuisine_type
+        def __init__(self, name, k_type, reiting):
+            self.name = name
+            self.k_type = k_type
             self.reiting = reiting
-        def describe_restaurant(self):
-            print(f'Название ресторана: {self.restaurant_name} Тип кухни: {self.cuisine_type}')
+        def d_restaurant(self):
+            print(f'Название ресторана: {self.name} Тип кухни: {self.k_type}')
         def open_restaurant(self):
             print('Ресторан открыт!')
-        def write_reiting(self):
+        def w_reiting(self):
             print(fr'Начальный рейтинг : {self.reiting}')
             self.reiting = input('Новый рейтинг: ')
             print(fr'Обновленный рейтинг: {self.reiting}')
     newRestaurant = Restaurant('Jungle', 'азиатская', '5')
-    newRestaurant.write_reiting()
+    newRestaurant.w_reiting()
 if x==3:
     f3()
 if x< 0 or x > 3:
